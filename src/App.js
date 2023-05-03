5609cecbac576a7bc7f7b9b7e1503e489320a55f
rusel28 <ruslans3101@gmail.com>
2023-05-04 00:45:20 +0300

    get

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mantine/core';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+    let login = "sergei.stralenia@gmail.com"
+    let password = "paralect123"
+    let client_id = 2356
+    let client_secret = "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948"
+    const URL = `https://startup-summer-2023-proxy.onrender.com/2.0/oauth2/password/?login=${login}&password=${password}&client_id=${client_id}&client_secret=${client_secret}`
+
+    axios.get(URL, {
+        headers: {"x-secret-key": "GEU4nvd3rej*jeh.eqp"}
+    })
+        .then(res => {
+            console.log("Authorization", res);
+        })
+
+
+    axios.get(`https://startup-summer-2023-proxy.onrender.com/2.0/catalogues/?login=${login}&password=${password}&client_id=${client_id}&client_secret=${client_secret}`, {
+        headers: {"x-secret-key": "GEU4nvd3rej*jeh.eqp"}
+    })
+        .then(res => {
+            console.log("catalogues", res.data);
+        })
 }
 
 export default App;
